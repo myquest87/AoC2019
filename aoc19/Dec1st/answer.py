a7a706b0ee76d7423391110c1cb315633f3d705a
myquest87 <mike@mwtwest.com>
2019-12-01 18:13:36 +0000

    Added answer for dec1st part 2

--- aoc19/Dec1st/answer.py
+++ aoc19/Dec1st/answer.py
@@ -4,11 +4,12 @@ import numpy as np
 input_path = Path(__file__).parent.joinpath("input.txt")
 
 
-def _get_fuel_required(module_mass: int):
-    return np.floor(module_mass / 3) - 2
+def _get_fuel_required(mass: int):
+    return np.floor(mass / 3) - 2
 
 
-def main():
+def _get_answer_part_1():
+    # answer is 3367126
     with input_path.open() as input_file:
         total_fuel_required = 0
         for module_mass in input_file:
@@ -17,7 +18,32 @@ def main():
             fuel_required_for_module = _get_fuel_required(module_mass_as_int)
 
             total_fuel_required += fuel_required_for_module
-        print(total_fuel_required)
+    return total_fuel_required
+
+
+def _get_answer_part_2():
+    # answer is 5047796
+    with input_path.open() as input_file:
+        total_fuel_required = 0
+        for module_mass in input_file:
+            module_mass_as_int = int(module_mass)
+            fuel_required_for_module = _get_fuel_required(module_mass_as_int)
+
+            fuel_required_for_fuel = _get_fuel_required(fuel_required_for_module)
+            fuel_required_for_module += fuel_required_for_fuel
+
+            while fuel_required_for_fuel > 0:
+                fuel_required_for_fuel = _get_fuel_required(fuel_required_for_fuel)
+                if fuel_required_for_fuel > 0:
+                    fuel_required_for_module += fuel_required_for_fuel
+
+            total_fuel_required += fuel_required_for_module
+    return total_fuel_required
+
+
+def main():
+    answer = _get_answer_part_2()
+    print(answer)
 
 
 if __name__ == "__main__":
